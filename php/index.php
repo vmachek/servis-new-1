81e89d183538d83b09d2326a86e2743c269bb1ba
Vlastimil Machek <vlastimil@pocitacepribram.cz>
2022-02-19 23:48:49 +0100

    Add nav.php to structure

--- php/index.php
+++ php/index.php
@@ -4,12 +4,15 @@ session_start();
 /** DATABASE */
 require_once "config/database.php";
 
+/** HEADER */
 require_once "_partials/header.php";
 
+/** CONTENT */
 if (empty($_SESSION('logged'))) {
 	require_once "_partials/login.php";
 } else {
 	require_once "_partials/logged.php";
 }
 
+/** FOOTER */
 require_once "_partials/footer.php";
