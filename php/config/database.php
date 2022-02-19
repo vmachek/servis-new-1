35b0b475b5f10f10d19bbe4beaca15f7c834b7ec
Vlastimil Machek <vlastimil@pocitacepribram.cz>
2022-02-19 23:44:32 +0100

    Add config for Medoo

--- php/config/database.php
+++ php/config/database.php
@@ -1,4 +1,6 @@
 <?php
+// require_once "vendor/catfan/medoo/src/Medoo.php";
+
 // Require Composer's autoloader.
 require 'vendor/autoload.php';
 
@@ -9,13 +11,13 @@ $database = new Medoo([
 	// [required]
 	'type' => 'mariadb',
 	'host' => 'localhost',
-	'database' => 'name',
-	'username' => 'your_username',
-	'password' => 'your_password',
+	'database' => 'fimak_servis',
+	'username' => 'root',
+	'password' => '0139Sql.',
 
 	// [optional]
 	'charset' => 'utf8mb4',
-	'collation' => 'utf8mb4_general_ci',
+	'collation' => 'utf8mb4_czech_ci',
 	'port' => 3306,
 
 ]);
